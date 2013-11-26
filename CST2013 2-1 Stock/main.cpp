d884b3ced238a7ed5867f99b63c713aa7ba114c8
zero <boy2000_007man@163.com>
2013-11-26 21:37:33 +0800

    it works fine, except out of time.

--- CST2013 2-1 Stock/main.cpp
+++ CST2013 2-1 Stock/main.cpp
@@ -5,69 +5,49 @@ using namespace std;
 
 //  int(log10(2^31 - 1) + 1) + 1 + int(log10(10^3 - 1) + 1) + 1 = 15
 const int DX_LENGTH = 15;
-struct Stock {
-    int X;
-    Stock *prev, *next;
-};
-struct StockTop {
-    int X, num;
-    StockTop *prev, *next;
-};
-template <typename T>
-void deleteHead(T *head) {
-    head->next = head->next->next;
-    delete head->next->prev;
-    head->next->prev = head;
-}
-template <typename T>
-void addEnd(T *end) {
-    end->prev->next = new T;
-    end->prev->next->next = end;
-    end->prev->next->prev = end->prev;
-    end->prev = end->prev->next;
-}
+const int N_MAX = 10000000;
+int stockX[N_MAX];
+const int X_MAX = 1000; // avoid been full
+int topX[X_MAX],
+    topNum[X_MAX];
+
+#define mod(a, b) (((a) % (b) + (b)) % (b))
 int main() {
     int N;
     scanf("%d", &N);
 
     double sum = 0.0;
-    int currentDay = 0;
-    Stock *stockHead = new Stock, *stockEnd = new Stock;
-    stockHead->next = stockEnd;
-    stockEnd->prev = stockHead;
-    StockTop *topHead = new StockTop, *topEnd = new StockTop;
-    topHead->next = topEnd;
-    topEnd->prev = topHead;
-    topHead->next->X = 0;
-    for (int i = 0, lastDay = -1; i < N << 1; i++) {
+    int currentDay = 0,
+        stockHead = 0, stockEnd = 0,
+        topHead = 0, topEnd = 0;
+    for (int i = 0, lastDay; i < N << 1; i++) {
         char DX[DX_LENGTH];
         scanf(" %[^\n]", DX);
+
         currentDay += atoi(DX);
-        if (lastDay == -1)
-            lastDay = currentDay;
-        if (strchr(DX, ' ') == NULL) {
-            topHead->next->num--;
-            if (topHead->next->num == 0) {
-                sum += (currentDay - lastDay) * topHead->next->X;
+        if (!strchr(DX, ' ')) {
+            if (--topNum[topHead] == 0) {
+                sum += (double)(currentDay - lastDay) * topX[topHead];
                 lastDay = currentDay;
-                deleteHead(topHead);
+                topHead = mod(topHead + 1, X_MAX);
             }
-            deleteHead(stockHead);
+            stockHead++;
         } else {
-            addEnd(stockEnd);
-            stockEnd->prev->X = atoi(strchr(DX, ' '));
-            if (topHead->next->X < stockEnd->prev->X) {
-                sum += (currentDay - lastDay) * topHead->next->X;
+            stockX[stockEnd++] = atoi(strchr(DX, ' '));
+            if (topHead == topEnd)
+                lastDay = currentDay;
+            else if (topX[topHead] < stockX[stockEnd - 1]) {
+                sum += (double)(currentDay - lastDay) * topX[topHead];
                 lastDay = currentDay;
             }
             int num = 1;
-            while (topEnd->prev != topHead && topEnd->prev->X < stockEnd->prev->X) {
-                num += topEnd->prev->num;
-                deleteHead(topEnd->prev->prev);
+            while (topHead != topEnd && topX[mod(topEnd - 1, X_MAX)] <= stockX[stockEnd - 1]) {
+                topEnd = mod(topEnd - 1, X_MAX);
+                num += topNum[topEnd];
             }
-            addEnd(topEnd);
-            topEnd->prev->X = stockEnd->prev->X;
-            topEnd->prev->num = num;
+            topX[topEnd] = stockX[stockEnd - 1];
+            topNum[topEnd] = num;
+            topEnd = mod(topEnd + 1, X_MAX);
         }
     }
     printf("%.2lf\n", sum / currentDay);
