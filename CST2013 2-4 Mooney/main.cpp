3e59a4017dfd02d900278d819aa9106cbd1bcfad
zero <boy2000_007man@163.com>
2013-11-28 15:59:15 +0800

    wait for test.

--- CST2013 2-4 Mooney/main.cpp
+++ CST2013 2-4 Mooney/main.cpp
@@ -2,66 +2,78 @@
 using namespace std;
 
 const int n_MAX = 500000;
-int edge[n_MAX];
 char mM[n_MAX];
-int GG[n_MAX];
-int MM[n_MAX];
-int DFN[n_MAX];
-int LOW[n_MAX];
-int link[n_MAX];
-int set[n_MAX];
-int value[n_MAX];
+int edge[n_MAX], revEdge[n_MAX],
+    heap[n_MAX], reflection[n_MAX],
+    GG[n_MAX],
+    DFN[n_MAX], LOW[n_MAX], link[n_MAX],
+    set[n_MAX], value[n_MAX],
+    MM[n_MAX];
 const int m_MAX = 1200000;
-int point[m_MAX];
-int next[m_MAX];
-int stack[m_MAX];
+int point[m_MAX], next[m_MAX],
+    revPoint[m_MAX], revNext[m_MAX],
+    stack[n_MAX + m_MAX];
 
 #define moon(mM) ((mM) == 'M')
 #define money(mM) ((mM) == 'm')
 #define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) < (b) ? (b) : (a))
+template <typename T> void swap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
 int main() {
     int n, m;
     scanf("%d%d%s", &n, &m, mM);
     for (int i = 0; i < n; i++)
-        edge[i] = -1;
+        revEdge[i] = edge[i] = -1;
     for (int i = 0; i < m; i++) {
         int u, v;
         scanf("%d%d", &u, &v);
         point[i] = v;
         next[i] = edge[u];
         edge[u] = i;
+        revPoint[i] = u;
+        revNext[i] = revEdge[v];
+        revEdge[v] = i;
     }
 
+    int heapNum = 0;    // Dijkstra with binary heap, O((n + m) * log(n))
     GG[0] = moon(mM[0]);
     for (int i = 1; i < n; i++)
-        GG[i] = n;
-    int stackNum = 0;
-    stack[stackNum++] = 0;
-    while (stackNum) {
-        int u = stack[--stackNum];
+        GG[heap[reflection[i] = heapNum++] = i] = n;
+    for (int u = 0; u != n - 1; ) {
         for (int i = edge[u]; i != -1; i = next[i]) {
             int v = point[i];
             if (GG[u] + moon(mM[v]) < GG[v]) {
                 GG[v] = GG[u] + moon(mM[v]);
-                stack[stackNum++] = v;
+                for (int loc = reflection[v]; loc && GG[heap[loc]] < GG[heap[loc - 1 >> 1]]; swap(heap[loc = loc - 1 >> 1], heap[loc]))
+                    swap(reflection[heap[loc - 1 >> 1]], reflection[heap[loc]]);
             }
         }
+        u = heap[0];
+        reflection[heap[0] = heap[--heapNum]] = 0;
+        int min = 0, root;
+        do {
+            root = min;
+            for (int j = 0; j < 2; j++)
+                if ((root + 1 << 1) - j < heapNum && GG[heap[(root + 1 << 1) - j]] < GG[heap[min]])
+                    min = (root + 1 << 1) - j;
+            swap(reflection[heap[min]], reflection[heap[root]]);
+            swap(heap[min], heap[root]);
+        } while (root != min);
     }
     printf("%d\n", GG[n - 1]);
 
-    for (int i = 0; i < n; i++)
-        DFN[i] = -1;
-    for (int i = 0; i < n; i++)
-        set[i] = -1;
-    stackNum = 0;
+    for (int i = 0; i < n; i++) // Tarjan, O(n + m)
+        set[i] = DFN[i] = -1;
+    int stackNum = 0;
     stack[stackNum++] = 0;
-    int linkNum = 0;
-    for (int time = 0; time < n || linkNum; ) {
+    for (int time = 0, linkNum = 0; !time || linkNum; ) {
         int u = stack[stackNum - 1];
-        if (DFN[u] == -1) {
-            LOW[u] = DFN[u] = time++;
-            link[linkNum++] = u;
-        }
+        if (DFN[u] == -1)
+            LOW[link[linkNum++] = u] = DFN[u] = time++;
         bool end = true;
         for (int i = edge[u]; i != -1; i = next[i]) {
             int v = point[i];
@@ -71,40 +83,50 @@ int main() {
             } else if (set[v] == -1)
                 LOW[u] = min(LOW[u], LOW[v]);
         }
-        if (!end)
-            continue;
-        stackNum--;
-        if (LOW[u] == DFN[u]) {
-            value[u] = 0;
-            do {
-                int v = link[linkNum - 1];
-                value[u] += money(mM[v]);
-                set[v] = u;
-                if (u != v && edge[v] != -1) {
-                    int tmp = edge[v];
-                    while (next[tmp] != -1)
-                        tmp = next[tmp];
-                    next[tmp] = edge[u];
-                    edge[u] = edge[v];
+        if (end) {
+            stackNum--;
+            if (LOW[u] == DFN[u]) {
+                value[set[u] = u] = money(mM[u]);
+                while (u != link[--linkNum]) {
+                    int v = link[linkNum];
+                    value[set[v] = u] += money(mM[v]);
+                    if (revEdge[v] != -1) {
+                        int tmp = revEdge[v];
+                        while (revNext[tmp] != -1)
+                            tmp = revNext[tmp];
+                        revNext[tmp] = revEdge[u];
+                        revEdge[u] = revEdge[v];
+                    }
                 }
-            } while (u != link[--linkNum]);
-        }
-    }
-    for (int i = 0; i < n; i++)
-        if (set[i] == i)
-            MM[i] = value[i];
-    stackNum = 0;
-    stack[stackNum++] = 0;
-    while (stackNum) {
-        int u = stack[--stackNum];
-        for (int i = edge[u]; i != -1; i = next[i]) {
-            int v = point[i];
-            v = set[v];
-            if (set[u] != set[v] && MM[set[v]] < MM[set[u]] + value[set[v]]) {
-                MM[set[v]] = MM[set[u]] + value[set[v]];
-                stack[stackNum++] = v;
             }
         }
+    }
+
+    for (int i = 0; i < n; i++) // DP, O(n + m)
+        if (i == set[i])
+            MM[i] = -1;
+    MM[set[0]] = value[set[0]];
+    stackNum = 0;
+    stack[stackNum++] = set[n - 1];
+    while (stackNum) {
+        int V = stack[stackNum - 1];
+        bool end = true;
+        if (MM[V] == -1) {
+            for (int i = revEdge[V]; i != -1; i = revNext[i]) {
+                int U = set[revPoint[i]];
+                if (U != V) {
+                    if (MM[U] == -1) {
+                        stack[stackNum++] = U;
+                        end = false;
+                    } else if (end)
+                        MM[V] = max(MM[V], MM[U] + value[V]);
+                }
+            }
+        }
+        if (!end)
+            MM[V] = -1;
+        else
+            stackNum--;
     }
     printf("%d\n", MM[set[n - 1]]);
 
