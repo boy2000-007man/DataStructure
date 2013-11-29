09823b224388b2319754deeb0a6a4fec0bb1889f
zero <boy2000_007man@163.com>
2013-11-30 00:45:03 +0800

    add readme

--- CST2013 2-2 Zerone/main.cpp
+++ CST2013 2-2 Zerone/main.cpp
@@ -1,3 +1,10 @@
+/*****************************************************************
+* Author        : B
+* Email         : boy2000_007man@163.com
+* Last modified : 2013-11-30 00:24
+* Filename      : main.cpp
+* Description   : O(log(s01) * n)
+*****************************************************************/
 #include <cstdio>
 using namespace std;
 
@@ -15,19 +22,17 @@ struct Node {
 void addLeaf(Node *root, char s01[]) {
     Node *leaf = root;
     for (int i = 0; s01[i] != '\0'; i++) {
-        if (leaf->leaf[s01[i] - '0'] == NULL)
+        if (!leaf->leaf[s01[i] - '0'])
             leaf->leaf[s01[i] - '0'] = new Node(leaf);
         leaf = leaf->leaf[s01[i] - '0'];
     }
-    if (leaf->winner != -1)
-        return ;
-    leaf->winner = 1;
-    while (leaf = leaf->root) {
-        leaf->winner = 1;
-        for (int i = 0; i < 2; i++)
-            if (leaf->leaf[i] != NULL)
-                leaf->winner &= !leaf->leaf[i]->winner;
-    }
+    if (leaf->winner == -1)
+        do {
+            leaf->winner = 1;
+            for (int i = 0; i < 2; i++)
+                if (leaf->leaf[i])
+                    leaf->winner &= !leaf->leaf[i]->winner;
+        } while (leaf = leaf->root);
 }
 int main() {
     int n;
@@ -37,6 +42,7 @@ int main() {
     for (int i = 0, winner = -1; i < n; i++) {
         char s01[s01_LENGTH + 1];
         gets(s01);
+
         addLeaf(root, s01);
         if (winner == -1)
             printf("%s %d", WINNER[winner = root->winner], i + 1);
