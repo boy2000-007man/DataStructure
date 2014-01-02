428b1344f16013487b06d0d51ecc6d07abd79444
zero <boy2000_007man@163.com>
2014-01-02 23:54:48 +0800

    make it more efficient and short.

--- CST2013 3-1 翻扑克 Pokeface/main.cpp
+++ CST2013 3-1 翻扑克 Pokeface/main.cpp
@@ -1,3 +1,10 @@
+/*****************************************************************
+* Author        : B
+* Email         : boy2000_007man@163.com
+* Last modified : 2014-01-02 23:47
+* Filename      : main.cpp
+* Description   : O((p + q) * log(n))
+*****************************************************************/
 #include <cmath>
 #include <cstdio>
 #include <cstring>
@@ -5,36 +12,34 @@ using namespace std;
 
 //  (int)ceil(log2(10^6)) = 20
 const int n_MAX = 1 << 20;
-int number[n_MAX];
+int number[n_MAX << 1];
 bool turn[n_MAX << 1],
-     modified[n_MAX];
+     modified[n_MAX << 1];
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (b) : (a))
+#define left(loc) (((loc) << 1) + 1)
+#define right(loc) (((loc) << 1) + 2)
+#define middle(a, b) ((a) + (b) >> 1)
 int question(int loc, int l, int r, int i, int j) {
-    if (l + 1 == r)
-        return turn[loc];
-    if (!modified[loc] && l == i && r == j)
+    if (j <= l || r <= i)
+        return 0;
+    if (i <= l && r <= j) {
+        if (modified[loc]) {
+            number[loc] = question(left(loc), l, middle(l, r), l, r) + question(right(loc), middle(l, r), r, l, r);
+            modified[loc] = false;
+        }
         return turn[loc] ? r - l - number[loc] : number[loc];
-    if (modified[loc]) {
-        number[loc] = question((loc << 1) + 1, l, l + r >> 1, l, l + r >> 1) + question((loc << 1) + 2, l + r >> 1, r, l + r >> 1, r);
-        modified[loc] = false;
     }
-    int ans = 0;
-    if (i < l + r >> 1)
-        ans += question((loc << 1) + 1, l, l + r >> 1, i, min(l + r >> 1, j));
-    if (l + r >> 1 < j)
-        ans += question((loc << 1) + 2, l + r >> 1, r, max(l + r >> 1, i), j);
-    return turn[loc] ? j - i - ans : ans;
+    int ans = question(left(loc), l, middle(l, r), i, j) + question(right(loc), middle(l, r), r, i, j);
+    return turn[loc] ? min(r, j) - max(l, i) - ans : ans;
 }
 void modify(int loc, int l, int r, int i, int j) {
-    if (l == i && r == j)
+    if (i <= l && r <= j)
         turn[loc] = !turn[loc];
-    else {
+    else if (!(j <= l || r <= i)){
         modified[loc] = true;
-        if (i < l + r >> 1)
-            modify((loc << 1) + 1, l, l + r >> 1, i, min(l + r >> 1, j));
-        if (l + r >> 1 < j)
-            modify((loc << 1) + 2, l + r >> 1, r, max(l + r >> 1, i), j);
+        modify(left(loc), l, middle(l, r), i, j);
+        modify(right(loc), middle(l, r), r, i, j);
     }
 }
 int main() {
@@ -42,9 +47,9 @@ int main() {
     scanf("%d%d%d", &n, &p, &q);
 
     int nFull = 1 << (int)ceil(log2(n));
-    memset(number, 0, sizeof(*number) * nFull);
+    memset(number, 0, sizeof(*number) * (nFull << 1));
     memset(turn, 0, sizeof(*turn) * (nFull << 1));
-    memset(modified, 0, sizeof(*modified) * nFull);
+    memset(modified, 0, sizeof(*modified) * (nFull << 1));
     modify(0, 0, nFull, 0, n);
     for (int k = 0; k < p + q; k++) {
         char HQ;
