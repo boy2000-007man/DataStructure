0878a8f73d19c53af13459dfd098b682d50a6328
zero <boy2000_007man@163.com>
2014-01-04 11:35:49 +0800

    new version

--- CST2013 3-4 选矿 Beneficiation/main.cpp
+++ CST2013 3-4 选矿 Beneficiation/main.cpp
@@ -1,34 +1,38 @@
+/*****************************************************************
+* Author        : B
+* Email         : boy2000_007man@163.com
+* Last modified : 2014-01-04 00:31
+* Filename      : main.cpp
+* Description   : O(m * n * log(n))
+*****************************************************************/
 #include "beneficiation.h"
-#include <cstdio>
 #include <cstring>
 using namespace std;
 
 const int n_MAX = 10000;
-int no[n_MAX + 1];
-bool reported[n_MAX];
+int no[n_MAX],
+    left[n_MAX], leftNum;
 template <typename T> void swap(T &a, T &b) {
     T tmp = a;
     a = b;
     b = tmp;
 }
-#define Report(no, rank) {\
-    Report(no, rank);\
-    reported[rank] = true;\
-}
+#define Report(rank) Report(no[rank], rank)
 void sort(int l, int r) {
-    int result[4];
+    static int result[4];
     if (r - l == 1)
-        Report(no[l], l);
-    if (r - l == 3) {
+        Report(l);
+    else if (r - l == 2)
+        left[leftNum++] = l;
+    else if (r - l == 3) {
         Measure(no[l], no[l + 1], no[l + 2], -1, &result[0], &result[1], &result[2], &result[3]);
         for (int i = 0; i < 3; i++) {
             swap(no[l + i], no[l + 1 - result[i]]);
             swap(result[i], result[1 - result[i]]);
         }
         for (int i = l; i < r; i++)
-            Report(no[i], i);
-    }
-    if (r - l == 4) {
+            Report(i);
+    } else if (r - l == 4) {
         Measure(no[l], no[l + 1], no[l + 2], no[l + 3], &result[0], &result[1], &result[2], &result[3]);
         for (int i = 0; i < 4; i++)
             for (int j = i + 1; j < 4; j++)
@@ -36,8 +40,8 @@ void sort(int l, int r) {
                     swap(no[l + i], no[l + j]);
                     swap(result[i], result[j]);
                 }
-        Report(no[l], l);
-        Report(no[r - 1], r - 1);
+        Report(l);
+        Report(r - 1);
         sort(l + 1, r - 1);
     }
     if (r - l < 5)
@@ -46,63 +50,46 @@ void sort(int l, int r) {
     swap(no[l + 2], no[r - 1]);
     Measure(no[l], no[l + 1], no[l + 2], -1, &result[0], &result[1], &result[2], &result[3]);
     for (int i = 0; i < 3; i++) {
-        swap(no[l + i], no[l + 1 - result[i]]);
-        swap(result[i], result[1 - result[i]]);
+        swap(no[l + i], no[l + (result[i] + 3) % 3]);
+        swap(result[i], result[(result[i] + 3) % 3]);
     }
-    swap(no[l], no[l + 1]);
     int L = l + 2;
-    for (int R = l + 3, wait[3]; R < r; R += result[3] ? 3 : 2) {
-        for (int i = 0; i < 3; i++)
-            wait[i] = R + i < r ? R + i : n_MAX;
-        Measure(no[wait[0]], no[wait[1]], no[wait[2]], no[l], &result[0], &result[1], &result[2], &result[3]);
+    for (int R = l + 3; R < r; R += result[3] ? 3 : 2) {
+        Measure(no[R], R + 1 < r ? no[R + 1] : -1, R + 2 < r ? no[R + 2] : -1, no[l], &result[0], &result[1], &result[2], &result[3]);
         if (result[3])
-            for (int i = 0; i < 3 && wait[i] != n_MAX; i++)
+            for (int i = 0; i < 3 && R + i < r; i++)
                 result[i] = -result[3];
         else if (2 < r - R)
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 2; i++)
                 if (!result[i]) {
-                    swap(no[wait[i]], no[wait[2]]);
+                    swap(no[R + i], no[R + 2]);
                     swap(result[i], result[2]);
-                    break;
                 }
         for (int i = 0; i < 3 && result[i]; i++)
             if (result[i] == 1)
-                swap(no[L++], no[wait[i]]);
+                swap(no[L++], no[R + i]);
     }
     L--;
     swap(no[l], no[L]);
-    Report(no[L], L);
+    Report(L);
     sort(l, L);
     sort(L + 1, r);
 }
-void sort2(int n) {
-    int wait[4], result[4];
-    for (int i = 0; i < 4; i++)
-        wait[i] = n_MAX;
-    for (int loc = 0, waitNum = 4; waitNum == 4;) {
-        for (waitNum = 0; waitNum < 4 && loc < n; loc++)
-            if (!reported[loc])
-                wait[waitNum++] = loc;
-        if (waitNum) {
-            Measure(no[wait[0]], no[wait[1]], no[wait[2]], no[wait[3]], &result[0], &result[1], &result[2], &result[3]);
-            if (result[0] < result[1])
-                swap(no[wait[0]], no[wait[1]]);
-            if (waitNum == 4 && result[2] < result[3])
-                swap(no[wait[2]], no[wait[3]]);
-        }
-        for (int i = 0; i < waitNum; i++)
-            Report(no[wait[i]], wait[i]);
-    }
-}
-#undef Report
 int main() {
-    no[n_MAX] = -1;
     for (int n; n = GetNumOfOre(); ) {
         for (int i = 0; i < n; i++)
             no[i] = i;
-        memset(reported, 0, sizeof(reported));
+        leftNum = 0;
         sort(0, n);
-        sort2(n);
+        for (int i = 0, result[4]; i < leftNum; i += 2) {
+            Measure(no[left[i]], no[left[i] + 1], 1 < leftNum - i ? no[left[i + 1]] : -1, 1 < leftNum - i ? no[left[i + 1] + 1] : -1, &result[0], &result[1], &result[2], &result[3]);
+            for (int j = 0; j < 2 && i + j < leftNum; j++) {
+                if (result[j << 1] < result[(j << 1) + 1])
+                    swap(no[left[i + j]], no[left[i + j] + 1]);
+                Report(left[i + j]);
+                Report(left[i + j] + 1);
+            }
+        }
     }
 
     return 0;
